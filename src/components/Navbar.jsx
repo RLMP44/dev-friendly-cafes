0ff8a4cb1b49da62d2dbad0dc08aff086e50cbb2
Rachael Momber <rlmomber@gmail.com>
2024-03-12 19:10:35 +0900

    fix set keyword

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 
 import Searchbar from './Searchbar/Searchbar';
 
-function Navbar() {
+function Navbar({setKeyword}) {
   return (
     <div className="d-flex justify-content-between align-items-center w-100 py-2 px-4 bg-success-subtle">
       <div className="d-flex align-items-center">
@@ -11,7 +11,7 @@ function Navbar() {
         <h2>Matcha and keyboard</h2>
       </div>
       <div className="d-flex align-items-center gap-5">
-        <Searchbar />
+        <Searchbar setKeyword={setKeyword}/>
         <div className="dropdown">
           <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
             <i className="fa-solid fa-bars"></i>
