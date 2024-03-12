5d95f979ede77f511453b85943b9de55cb866a39
Rachael Momber <rlmomber@gmail.com>
2024-03-12 16:30:39 +0900

    connect and hello function working

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
 import './index.css';
+
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+// arrow function
+// const Hello = () => {}
+
+function Hello() {
+  return <h1>Hello!!</h1>;
+}
+
+const rootElement = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<Hello />);
