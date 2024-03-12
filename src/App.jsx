a9f14351a76db6f6c96dd1600bd01e493b963f31
Rachael Momber <rlmomber@gmail.com>
2024-03-12 17:02:10 +0900

    create app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
 // Backend API endpoint: https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes
+
+// export first so it can be rendered elsewhere
+export default function App() {
+  return(
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,25 @@ import './index.css';
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
+import App from "./App";
+
 // arrow function
 // const Hello = () => {}
 
-function Hello() {
-  return <h1>Hello!!</h1>;
-}
+// function Hello({name, age}) {
+//   return (
+//     // class reserved for JavaScript classes, so must be className for HTML
+//     // each react component can only return one HTML element
+//     // fragments <> </> are empty divs that you can use in react
+//     //<>
+//     <div className='container'>
+//       <h1>Hello {name} you are {age}</h1>
+//       <h2>Biiiiiiitch</h2>
+//     </div>
+//     // </>
+//   );
+// }
 
 const rootElement = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootElement);
-root.render(<Hello />);
+root.render(<App />);
